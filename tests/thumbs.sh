cbd1f8b467c24c49b84f9408b1c642e9dcf04ab0
Brigham Brown <me@brighambrown.com>
2025-09-01 11:19:38 -0700

    Simple test.

---
+++ tests/thumbs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Run the program and make sure it generates the expected thumbnails.
+
+
+set -e
+
+if [[ -e red_ring_*.png ]]
+then
+    rm red_ring_*.png
+fi
+
+uv run main.py --end-frame=1801 --scale-down=10 --mode=thumbs
+
+for file in tests/goldens/red_ring_*.png; do
+    cmp "$(basename "$file")" "$file"
+done
