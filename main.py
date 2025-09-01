b250a1beb02b1d7bd9d8495e8d57ecd7c237c811
Brigham Brown <me@brighambrown.com>
2025-09-01 08:49:39 -0700

    Add arguments for some configuration.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from PIL import Image, ImageChops, ImageDraw, ImageFilter
 import math
 import ffmpeg
@@ -8,8 +9,22 @@ import abc
 from typing import Tuple, List
 import subprocess
 from dataclasses import dataclass, field
+import argparse
 
 # https://youtu.be/a4Yge_o7XLg?si=YYmPQBmLYXq4cSoY at 1:10:30
+
+class Mode(str, Enum):
+    FULL = 'full'
+    VIDEO = 'video'
+    THUMBS = 'thumbs'
+
+    @property
+    def enable_video(self) -> bool:
+        return self != Mode.THUMBS
+
+    @property
+    def enable_thumbs(self) -> bool:
+        return self != Mode.VIDEO
 
 @dataclass
 class Config:
@@ -22,6 +37,9 @@ class Config:
     SECONDS: int = 2
     SKIP: int = 18
     LEVELS: int = 53
+    START_FRAME_BASE: int = 0
+    END_FRAME_BASE: int = -1
+    MODE: Mode = Mode.FULL
     COLORS0: List[str] = field(default_factory=lambda: ['blue', 'green', 'yellow', 'red'])
     COLORS1: List[str] = field(default_factory=lambda: ['cyan', 'yellow', 'orange', 'magenta'])
 
@@ -41,6 +59,32 @@ class Config:
     def FRAMES(self) -> int:
         return int(self.SECONDS * self.FPS * self.SKIP)
 
+    @property
+    def START_FRAME(self) -> int:
+        if self.START_FRAME_BASE < 0:
+            raise ValueError("START_FRAME_BASE must be non-negative")
+        if self.START_FRAME_BASE >= self.FRAMES:
+            raise ValueError("START_FRAME_BASE must be less than FRAMES")
+        result = self.START_FRAME_BASE
+        return result // self.SKIP * self.SKIP
+
+    @property
+    def END_FRAME(self) -> int:
+        if self.END_FRAME_BASE < -1:
+            raise ValueError("END_FRAME_BASE must be non-negative")
+        if self.END_FRAME_BASE > -1 and self.END_FRAME_BASE < self.START_FRAME:
+            raise ValueError(f"END_FRAME_BASE must be greater than or equal to START_FRAME ({self.START_FRAME})")
+        if self.END_FRAME_BASE > self.FRAMES:
+            raise ValueError("END_FRAME_BASE must be less than FRAMES")
+        result = self.END_FRAME_BASE if self.END_FRAME_BASE != -1 else self.FRAMES
+
+        is_off = result % self.SKIP != 0
+
+        return (result // self.SKIP * self.SKIP) + self.SKIP if is_off else result
+
+    @property
+    def DURATION(self) -> float:
+        return self.END_FRAME - self.START_FRAME
 
 class AbstractVideoProducer:
     def __init__(self, output_path: str, size: Tuple[int, int], fps: int):
@@ -53,6 +97,16 @@ class AbstractVideoProducer:
         pass
 
     @abc.abstractmethod
+    def finalize(self):
+        pass
+
+class NoopProducer(AbstractVideoProducer):
+    def __init__(self):
+        super().__init__("", (0, 0), 0)
+
+    def add_frame(self, frame: Image.Image, number: int):
+        pass
+
     def finalize(self):
         pass
 
@@ -186,16 +240,33 @@ def ncircles(disc_radius, radius):
 def minutes(m):
     return m * 60
 
-def main():
-    config = Config()
-    # Delete old png files
-    for f in itertools.chain(glob.glob('red_ring*.png'), glob.glob('red_ring*.bmp')):
-        os.remove(f)
+def parse_args() -> Config:
+    parser = argparse.ArgumentParser(description='Create a video with a rotating ring.')
+    parser.add_argument('--mode', type=Mode, default=Mode.FULL, choices=[mode.value for mode in Mode], help='Mode of operation')
+    parser.add_argument('--start-frame', type=int, default=0, help='Start frame (inclusive)')
+    parser.add_argument('--end-frame', type=int, default=-1, help='End frame (exclusive, -1 is default)')
+    parser.add_argument('--scale-down', type=int, default=1, help='Scale down factor')
+
+    parsed = parser.parse_args()
+    return Config(
+        MODE=parsed.mode,
+        START_FRAME_BASE=parsed.start_frame,
+        END_FRAME_BASE=parsed.end_frame,
+        SCALE_DOWN=parsed.scale_down
+    )
+
+def create_video(config: Config):
+    print(f"Creating video with {config}")
     try:
-        # rpm = 100
-        thumb_producer = GlobVideoProducer("thumbnails.mp4", config.CANVAS_SIZE, config.FPS, "red_ring")
-        producer = FFmpegVideoProducer("output.mp4", config.CANVAS_SIZE, config.FPS)
-        for add_rot in range(0, config.FRAMES, config.SKIP):
+        thumb_producer = producer = NoopProducer()
+        if config.MODE.enable_thumbs:
+            thumb_producer = GlobVideoProducer("thumbnails.mp4", config.CANVAS_SIZE, config.FPS, "red_ring")
+        if config.MODE.enable_video:
+            producer = FFmpegVideoProducer("output.mp4", config.CANVAS_SIZE, config.FPS)
+        start_frame = config.START_FRAME
+        end_frame = config.END_FRAME
+        print(f"Frames: {start_frame} to {end_frame}")
+        for add_rot in range(start_frame, end_frame, config.SKIP):
             add_rot_f = add_rot / 2
             image = Image.new("RGB", config.CANVAS_SIZE, (0, 0, 0))
             draw = ImageDraw.Draw(image)
@@ -236,6 +307,13 @@ def main():
         producer.finalize()
     except Exception as e:
         print(f"An error occurred: {e}")
+
+def main():
+    config = parse_args()
+    # Delete old png files
+    for f in itertools.chain(glob.glob('red_ring*.png'), glob.glob('red_ring*.bmp')):
+        os.remove(f)
+    create_video(config)
 
 if __name__ == "__main__":
     main()
